115a5c67528e0f318a85753e85d642de8787eae0
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2025-12-06 23:10:28 -0500

    0.0.6

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v2.0.62/---simulator","cdnUrl":"https://cdn.makecode.com","version":"0.0.5","target":"arcade","targetVersion":"2.0.62","repo":"robo-technical-group/pxt-tutorials"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v2.0.62/---simulator","cdnUrl":"https://cdn.makecode.com","version":"0.0.6","target":"arcade","targetVersion":"2.0.62","repo":"robo-technical-group/pxt-tutorials"}
 // total=1648043 new=28.72% cached=66.96% other=4.31%
 (function (ectx) {
 'use strict';
@@ -428,7 +428,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P160411(s) {
+function _main___P160422(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -830,13 +830,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P160411.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P160411.continuations = [  ]
+_main___P160422.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P160422.continuations = [  ]
 
-function _main___P160411_mk(s) {
+function _main___P160422_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P160411, depth: s.depth + 1,
+        parent: s, fn: _main___P160422, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42058,5 +42058,5 @@ const sprites_BaseSpriteSayRenderer__C144845_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P160411
+return _main___P160422
 })
